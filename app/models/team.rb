368128d34f536cdca28792bc73601ec22d24834b
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-07 17:03:28 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,19 +1,19 @@
 class Team
- +  attr_accessor :name, :motto
- +
- +  @@all = []
- +
- +  def initialize(params)
- +    @name = params[:name]
- +    @motto = params[:motto]
- +    @@all << self
- +
- +    params[:members].each do |member|
- +      SuperHero.new(member)
- +    end
- +  end
- +
- +  def self.all
- +    @@all
- +  end
- +end 
+  attr_accessor :name, :motto
+  @@all = []
+
+  def initialize(name, motto)
+    @name = params[:name]
+    @motto = params[:motto]
+    @@all << self
+
+    params[:member].each do |member|
+      Superhero.new(member)
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+end
