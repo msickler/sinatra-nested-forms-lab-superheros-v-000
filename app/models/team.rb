69e7ac98aedd632f35467eabed5b2dc0719bd9f2
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-07 17:05:33 +0000

    Done.

---
+++ app/models/team.rb
@@ -0,0 +1,18 @@
+class Team
+    attr_accessor :name, :motto
+    @@all = []
+
+   def initialize(params)
+     @name = params[:name]
+     @motto = params[:motto]
+     @@all << self
+
+     params[:members].each do |member|
+       SuperHero.new(member)
+     end
+    end
+
+   def self.all
+     @@all
+   end
+ end
